4b63f7ff16c0cd752e58b4584c29cbea56e99f39
Braden031401 <rc179978@students.davenport.k12.ia.us>
2017-10-03 08:08:44 -0500

    Chapter 8!

---
+++ Chapter-8/src/practice/ArrayPractice.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class ArrayPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double [] numbers = {23, 6, 47, 35, 2, 14};
+		
+		
+
+	}
+
+}
