988cb1fa6ec7b11e405520af9768cde91215d753
Braden031401 <rc179978@students.davenport.k12.ia.us>
2017-10-04 08:32:48 -0500

    Mastering Arrays, one double at a time...

--- Chapter-8/src/practice/ArrayPractice.java
+++ Chapter-8/src/practice/ArrayPractice.java
@@ -8,9 +8,64 @@ public class ArrayPractice {
 		double [] numbers = {23, 6, 47, 35, 2, 14};
 		double Average;
 		
-		Average
 		
-
+		Average = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] / 6;
+		
+		System.out.println("The average is "+ Average);
+		
+		if( numbers[0] > numbers[1] & numbers[0] > numbers[2] & numbers[0] > numbers[3] & numbers[0] > numbers[4] & numbers[0] > numbers[5])	
+		{
+			System.out.println("The greatest number is "+ numbers[0]);
+		}
+		if( numbers[1] > numbers[0] & numbers[1] > numbers[2] & numbers[1] > numbers[3] & numbers[1] > numbers[4] & numbers[1] > numbers[5])	
+		{
+			System.out.println("The greatest number is "+ numbers[1]);
+		}
+		if( numbers[2] > numbers[1] & numbers[2] > numbers[0] & numbers[2] > numbers[3] & numbers[2] > numbers[4] & numbers[2] > numbers[5])	
+		{
+			System.out.println("The greatest number is "+ numbers[2]);
+		}
+		if( numbers[3] > numbers[1] & numbers[3] > numbers[2] & numbers[3] > numbers[0] & numbers[3] > numbers[4] & numbers[3] > numbers[5])	
+		{
+			System.out.println("The greatest number is "+ numbers[3]);
+		}
+		if( numbers[4] > numbers[1] & numbers[4] > numbers[2] & numbers[4] > numbers[3] & numbers[4] > numbers[0] & numbers[4] > numbers[5])	
+		{
+			System.out.println("The greatest number is "+ numbers[4]);
+		}
+		if( numbers[5] > numbers[1] & numbers[5] > numbers[2] & numbers[5] > numbers[3] & numbers[5] > numbers[4] & numbers[5] > numbers[0])	
+		{
+			System.out.println("The greatest number is "+ numbers[5]);
+		}
+		
+		
+		
+		
+		if ( numbers[0] % 2 != 0 )
+		{
+			System.out.println(numbers[0] +" is odd");
+		}
+		
+		if( numbers[1] % 2 != 0 )
+		{
+			System.out.println(numbers[1] +" is odd ");
+		}
+		if( numbers[2] % 2 != 0 )
+		{
+			System.out.println(numbers[2] +" is odd");
+		}
+		if( numbers[3] % 2 != 0 )
+		{
+			System.out.println(numbers[3] +" is odd ");
+		}
+		if( numbers[4] % 2 != 0 )
+		{
+			System.out.println(numbers[4] +" is odd ");
+		}
+		if( numbers[5] % 2 != 0 )
+		{
+			System.out.println(numbers[5] +" is odd ");
+		}
 	}
 
 }
---
+++ Chapter-8/src/practice/Page430Questions.java
@@ -0,0 +1,34 @@
+package practice;
+
+public class Page430Questions {
+/* 1 - An array is a list of data items that: Have the same type.
+  
+   2 - When you declare an array: You always reserve memory for it in the same statement.
+
+   4 - For how many integers does the following statement reserve room? int[] value = new int[34]: 0
+   
+   5 - Which of the following can be used as an array subscript: int?
+   
+   6 - If you declare an array as follows, how do you indicate the final element of the array? int[] num = new int[6]: impossible to tell
+   
+   7 - If you declare an integer array as follows, what is the value of num[2]: 303
+   
+   8 - Array names represent: References
+   
+   12 - Assume an array is declared as follows. Which of the following statements correctly assigns the value 100 to each of the array elements: for(x=0; x<4; ==x) num[x] = 100
+   
+   13 - Suppose you have declared an array as follows - int[] creditScores = {670, 720, 815} - What is the value of creditScores.length: 2
+   
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	*/
+}
